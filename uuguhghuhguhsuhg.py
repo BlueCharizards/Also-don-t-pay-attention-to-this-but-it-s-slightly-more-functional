926723a49487f714accea6c30464566bd564820c
BlueCharizards <1keenansil@hdsb.ca>
2017-07-21 15:31:41 -0400

    Add files via upload

---
+++ uuguhghuhguhsuhg.py
@@ -0,0 +1,280 @@
+"""cost = 50
+tax = 0.13
+tip = 0.15
+subtotal = cost + (cost * tax)
+total = subtotal + (subtotal * tip)
+print (total)"""
+
+"""name = "Stop"
+name = "help me please I'm dying"
+print (name)
+hi = "hi"
+print (len(name))
+thanks = "you're welcome"
+Philip = "is a smol bean"
+print (Philip.upper()) 
+input ("HI HOW ARE YA DOING? ")
+feelings = input"""
+
+"""number1 == number2 #equals to
+number1 != number2 #not equal to
+number1 < number2 #is less than
+number1 > number2 #is more than
+number1 <= number2 #is less than or equal to
+number1 >= number2 #is more than or equal to"""
+
+"""value1 = False
+value2 = False
+value3 = False
+value4 = True
+value5 = False
+False False False False False True"""
+"""print (thanks)"""
+
+#and or not
+"""WHAT CAN I SAY BUT HEY YOU'RE WELCOME"""
+
+value1 = False
+value2 = False
+value3 = True
+#False 0 is less than or equal to 10x2=20 20-20=0
+
+"""YES I did life correctly!!!!!!!
+Hahanope I'm actually dumb
+donate today to help children with crippling low self esteem"""
+
+"""input ("WHAT IS YOUR FAVOURITE COLOUR? ")
+bluenogreen = input
+blue = 1
+green = 2
+purple = 3
+pink = 4
+red = 5
+yellow = 6
+orange = 7
+no = 8
+Nicholas = "hahahahahahahahahahahahaha"
+input ("What am I doing with my life ")
+absolutelynothing = input"""
+
+"""yeah no I'm really not doing anything it's true"""
+"""OR or or or or or or orange or or or or or or or help"""
+"""help is purple that's cool"""
+
+value1 = False
+value2 = False
+value3 = True
+
+"""input ("Pardon me, are you Aaron Burr, sir? ")
+Lin = input
+input ("Oh well sure, sir, I'm Alexander Hamilton I'm at you service, sir, I have been looking for you ")
+Manuel = input
+input ("Sir, I heard your name at Princeton I was seeking an accelerated course of study, when I got sort of out of sorts with a buddy of yours. I may have punched him, it's a blur, sir, he handles the financials? ")
+Miranda = input"""
+
+value1 = True
+value2 = True
+value3 = False
+
+"""uuuuhhhgnnnnnsnnnsnnnsngsnnshh"""
+"""I'm too STUPID"""
+"""okay so what do I do now?"""
+"""Continue to be pathetic OKAY SHOULDN'T BE HARD"""
+
+"""if 3 == 2 + 1:
+    print ("hahawhat")
+elif name == "Philip"
+    print ("THE SMOLLLEST SMOL BEAN")
+else:
+    print ("wooooow you suck get out")"""
+
+#and, or, not
+# use a comparator (==, >=, <=, >, <, !=)
+# print True
+
+"""Christmas = 25
+NOPE = 30
+yes = 25"""
+"""if Christmas == NOPE:
+    print ("Christmas is cancelled kids Santa died."
+elif:
+    5 + 10 != 4 + 5 and (5**20) + 52 > 2*2
+        print ("True")
+else:
+
+    print ("Hey guess what Santa can't die! Because he isn't real. Have fun with your crushed dreams.")
+    
+name = "Philip"""
+
+# % is modulo (the remainder after division)
+"""gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg"""
+
+
+"""ay = "ay"
+Lies = input ("Thank you for using the newest version of Google translate! Enter a word now to translate it into any language in the world! ")
+if len(Lies) > 0 and Lies.isalpha():
+    first_letter = Lies[0]
+    final_word = Lies + first_letter + ay
+    final_word = final_word[1:len(final_word)]
+    print (final_word)
+    print ("Haha tricked you it only does pig latin")
+else:
+    print ("Okay so you're actually supposed to write a WORD with LETTERS but it's fine if you can't. Actually it isn't.")"""
+        
+"""def square(x):
+    return x**2"""
+
+"""def five(x):
+    return x + 5
+
+def three(y):
+    return five(y) - 3"""
+
+#define a function that takes a number and returns the cube of that number.
+#define another funcion that takes a number as an argument.
+#if that number is divisible by 3, it calls the first function.
+#otherwise, it returns False.
+
+def cube(z):
+    return z**3
+
+def three(a):
+    if (a)%3 == 0:
+        return cube(a)
+    else:
+        return False
+
+"""did I do the thing right? Because I don't know how to make it do the other function but it still does what it's supposed to I think?"""
+"""WAIT NO I DIDN'T I FORGOT THE OTHER THING"""
+"""Okay crisis averted I did it and it works for once but I don't know how to actually do a function inside another function and I think that was the point"""
+"""IT ACTUALLY WORKS IT'S A MIRACLE I CAN'T BELIEVE IT WHAT DID I DO HOOOWWWWWWWWWW I'M NOT STUPID I HAVE PROOF NOW"""
+
+import math
+
+print (math.sqrt(562))
+    
+maximum = max(3, 43, 54, 7, 9)
+print (maximum)
+
+minimum = min(3, 6, 4, 9, 1)
+print (minimum)
+
+
+absolute = abs(-35)
+print (absolute)
+
+print(type(42))
+
+
+#create a function that calculates and returns your hotel costs
+#the cost per night is $150.
+#create a function that calculates the cost depending on the city you go to.
+"""City Costs
+Chicago $183
+London $222
+Toronto $220
+Tokyo $475"""
+
+"""So I'm gonna say my vacation is a week long because that seems reasonable so 7 nights"""
+
+def hotel(e):
+    return e * 150
+
+Chicago = 183
+London = 222
+Toronto = 220
+Tokyo = 475
+def City(c):
+    if c == "Chicago":
+        return Chicago
+    elif c == "London":
+        return London
+    elif c == "Toronto":
+        return Toronto
+    elif c == "Tokyo":
+        return Tokyo
+    else:
+        print ("you can't go there!")
+
+"""OR"""
+
+def plane_cost(city):
+    if city == Chicago:
+        return 183
+    elif city == Toronto:
+        return 220
+"""etc."""
+
+#Define another function that calculates and returns the cost of renting a car.
+#The cost per day is $150. If you rent the car for 7 or more days you get a $50 discount.
+#If you rent the car for 3 or more days, you get a $20 discount.
+
+"""def car(days):
+    if days >= 7:
+        return days*150-50
+    elif days >= 3:
+        return days*150-20
+    elif days < 3 and days > 0:
+        return days*150
+    else:
+        return ("You cannot rent a car for less than one day(s). Please choose a valid number. You idiot.")
+
+#Create a function that prins the sum of calling the three functions that you created.
+
+def total_cost():
+    city = input ("Which city are you flying to? ")
+    days = int(input ("How long are you staying there? "))
+    if city == "Chicago" or city == "London" or city == "Toronto" or city == "Tokyo" and days:
+        total = City(city) + hotel(days) + car(days)
+        print (total) 
+    else:
+        print("You're stupid. You're really stupid. Now go to the corner and think about what you've done. Also enter a city that's on the list.")
+        total_cost()
+
+total_cost()"""
+
+#uuuuughhuuughgfsfgghuhg Modify the total_cost function so that instead of requiring arguments, it asks the user for the city and the number of days
+#If the user enters a city not on our list, the program should ask the user to enter a valid city instead of giving an error.
+
+numbers = [2,5,6,7,9,10,43]
+
+print (numbers[6])
+numbers[4] = 637
+numbers.append(98765432109876543210987654)
+print(numbers)
+#numbers.append adds a number to the end of the list
+
+letters = ["a", "b", "c", "d", "e", "f"]
+
+sliced_list = letters[2:5]
+print (sliced_list)
+#sliced_list prints only some letters
+
+animals = ["ant", "bat", "cat", "dog"]
+
+print(animals.index("cat"))
+
+animals.insert(1, "snake")
+
+print (animals)
+
+#ant, snake, bat, cat, dog
+
+animals.pop(2)
+#animals.pop removes an item based on inde
+
+print (animals)
+
+animals.remove("cat")
+
+print (animals)
+
+animals.sort()
+
+print (animals)
+
+#Create a function that takes a list and prints the first item in that list
+
+def first(list1):
+    print(list1[0])
+#########ITDIDATHING#########
